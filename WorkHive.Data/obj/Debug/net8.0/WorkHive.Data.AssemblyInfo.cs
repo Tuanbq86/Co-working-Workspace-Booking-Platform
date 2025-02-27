7ad8c4259f6537f3059d30c33f7822021c79c6dc
Kobii24 <quochuyho220603@gmail.com>
2025-02-28 00:20:35 +0700

    update huy branch

--- WorkHive.Data/obj/Debug/net8.0/WorkHive.Data.AssemblyInfo.cs
+++ WorkHive.Data/obj/Debug/net8.0/WorkHive.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkHive.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6f2b69e61a653f9b1781291c7c617153f8e2de4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+767e125b5cba1fc5635f9243f0e714c19b5b322c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkHive.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkHive.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
