64380cb29226bd1f80bcb2887b5ab2288d5113f4
Kobii24 <quochuyho220603@gmail.com>
2025-03-02 23:35:38 +0700

    Done getBookingList api

---
+++ WorkHive.Services/BookingDTO/BookingHistory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkHive.Services.BookingDTO;
+
+public sealed class BookingHistory
+{
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public decimal Price { get; set; }
+    public string? Status { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
