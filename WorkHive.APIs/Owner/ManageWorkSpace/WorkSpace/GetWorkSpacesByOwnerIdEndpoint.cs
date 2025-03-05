bbcec2a7345598a3f1190caef039a692f1eac61e
Kobii24 <quochuyho220603@gmail.com>
2025-03-05 15:55:23 +0700

    Fix api get all workspace by ownerId

--- WorkHive.APIs/Owner/ManageWorkSpace/WorkSpace/GetWorkSpacesByOwnerIdEndpoint.cs
+++ WorkHive.APIs/Owner/ManageWorkSpace/WorkSpace/GetWorkSpacesByOwnerIdEndpoint.cs
@@ -1,28 +1,28 @@
-﻿//using Carter;
-//using MediatR;
-//using WorkHive.Services.Owners.ManageWorkSpace.GetAllById;
+﻿using Carter;
+using MediatR;
+using WorkHive.Services.Owners.ManageWorkSpace.GetAllById;
 
-//namespace WorkHive.APIs.Owner.ManageWorkSpace.WorkSpace
-//{
-//    public record GetWorkSpacesByOwnerIdResponse(List<GetWorkSpaceByOwnerIdResult> Workspaces);
+namespace WorkHive.APIs.Owner.ManageWorkSpace.WorkSpace
+{
+    public record GetWorkSpacesByOwnerIdResponse(List<GetWorkSpaceByOwnerIdResult> Workspaces);
 
-//    public class GetWorkSpacesByOwnerIdEndpoint : ICarterModule
-//    {
-//        public void AddRoutes(IEndpointRouteBuilder app)
-//        {
-//            app.MapGet("/workspaces/owner/{ownerId}", async (int ownerId, ISender sender) =>
-//            {
-//                var query = new GetWorkSpacesByOwnerIdQuery(ownerId);
-//                var result = await sender.Send(query);
-//                var response = new GetWorkSpacesByOwnerIdResponse(result);
+    public class GetWorkSpacesByOwnerIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/workspaces/owner/{Id}", async (int Id, ISender sender) =>
+            {
+                var query = new GetWorkSpacesByOwnerIdQuery(Id);
+                var result = await sender.Send(query);
+                var response = new GetWorkSpacesByOwnerIdResponse(result);
 
-//                return Results.Ok(response);
-//            })
-//            .WithName("GetWorkSpacesByOwnerId")
-//            .Produces<GetWorkSpacesByOwnerIdResponse>(StatusCodes.Status200OK)
-//            .ProducesProblem(StatusCodes.Status404NotFound)
-//            .WithSummary("Get Workspaces by Owner ID")
-//            .WithDescription("Retrieve all workspaces belonging to a specific owner.");
-//        }
-//    }
-//}
+                return Results.Ok(response);
+            })
+            .WithName("GetWorkSpacesByOwnerId")
+            .Produces<GetWorkSpacesByOwnerIdResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Get Workspaces by Owner ID")
+            .WithDescription("Retrieve all workspaces belonging to a specific owner.");
+        }
+    }
+}
