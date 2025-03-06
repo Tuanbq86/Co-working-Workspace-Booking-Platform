15883033070118e85e277b3a43354d5bda4a4569
Kobii24 <quochuyho220603@gmail.com>
2025-03-06 21:12:06 +0700

    update booking api

--- WorkHive.Services/Users/UpdateUser/UpdateUserPasswordHandler.cs
+++ WorkHive.Services/Users/UpdateUser/UpdateUserPasswordHandler.cs
@@ -8,24 +8,23 @@ using WorkHive.Services.Exceptions;
 namespace WorkHive.Services.Users.UpdateUser;
 
 public record UpdateUserPasswordCommand
-    (string OldPassword, string NewPassword, string ConfirmPassword) 
+    (int UserId, string OldPassword, string NewPassword, string ConfirmPassword) 
     : ICommand<UpdateUserPasswordResult>;
 public record UpdateUserPasswordResult(string Notification);
 
-public class UpdateUserPasswordHandler(IUserUnitOfWork userUnit, 
-    ITokenRepository tokenRepo, IHttpContextAccessor httpContext)
+public class UpdateUserPasswordHandler(IUserUnitOfWork userUnit)
     : ICommandHandler<UpdateUserPasswordCommand, UpdateUserPasswordResult>
 {
     public async Task<UpdateUserPasswordResult> Handle(UpdateUserPasswordCommand command, 
         CancellationToken cancellationToken)
     {
         //Get userId and roleId for Booking in session containing token in a session working
-        var token = httpContext.HttpContext!.Session.GetString("token")!.ToString();
-        var listInfo = tokenRepo.DecodeJwtToken(token);
+        //var token = httpContext.HttpContext!.Session.GetString("token")!.ToString();
+        //var listInfo = tokenRepo.DecodeJwtToken(token);
 
-        var userId = listInfo[JwtRegisteredClaimNames.Sub];
+        //var userId = listInfo[JwtRegisteredClaimNames.Sub];
 
-        var user = userUnit.User.GetById(Convert.ToInt32(userId));
+        var user = userUnit.User.GetById(command.UserId);
 
         //Check user password with old password in command
         if (BCrypt.Net.BCrypt.EnhancedVerify(command.OldPassword, user.Password))
