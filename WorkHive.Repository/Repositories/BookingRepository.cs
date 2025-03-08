4d1fb230e7e670b61830e1e7231d4a19a562fda9
Kobii24 <quochuyho220603@gmail.com>
2025-03-08 14:48:06 +0700

    update api getListBookingHistory

--- WorkHive.Repository/Repositories/BookingRepository.cs
+++ WorkHive.Repository/Repositories/BookingRepository.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
 using WorkHive.Data.Base;
 using WorkHive.Data.Models;
 using WorkHive.Repositories.IRepositories;
@@ -14,6 +10,18 @@ public class BookingRepository : GenericRepository<Booking>, IBookingRepository
     public BookingRepository() { }
     public BookingRepository(WorkHiveContext context) => _context = context;
 
-    //To do object method
+    public async Task<List<Booking>> GetAllBookingByUserId(int userId)
+    {
+        return await _context.Bookings.Include(b => b.Payment)
+                                .Include(b => b.WorkspaceTimes)
+                                .Include(b => b.Workspace)
+                                .Include(b => b.Promotion)
+                                .Include(b => b.BookingAmenities)
+                                .ThenInclude(ba => ba.Amenity)
+                                .Include(b => b.BookingBeverages)
+                                .ThenInclude(bb => bb.Beverage)
+                                .AsNoTracking() // optimize efficiency
+                                .ToListAsync();
+    }
 
 }
--- WorkHive.Services/Users/DTOs/BookingHistory.cs
+++ WorkHive.Services/Users/DTOs/BookingHistory.cs
@@ -1,16 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WorkHive.Services.Users.DTOs;
+﻿namespace WorkHive.Services.Users.DTOs;
 
 public sealed class BookingHistory
 {
-    public DateTime StartDate { get; set; }
-    public DateTime EndDate { get; set; }
-    public decimal Price { get; set; }
-    public string? Status { get; set; }
-    public DateTime CreatedAt { get; set; }
+    public DateTime Booking_StartDate { get; set; }
+    public DateTime Booking_EndDate { get; set; }
+    public decimal? Booking_Price { get; set; }
+    public string? Booking_Status { get; set; }
+    public DateTime Booking_CreatedAt { get; set; }
+    public string? Payment_Method { get; set; }
+    public string? Workspace_Name { get; set; }
+    public int? Workspace_Capacity { get; set; }
+    public string? Workspace_Category { get; set; }
+    public string? Workspace_Description { get; set; }
+    public int? Workspace_Area { get; set; }
+    public int? Workspace_CleanTime { get; set; }
+    public string? Promotion_Code { get; set; }
+    public int? Discount { get; set; }
+    public List<BookingHistoryAmenity>? BookingHistoryAmenities { get; set; }
+    public List<BookingHistoryBeverage>? BookingHistoryBeverages { get; set; }
 }
+
+public record BookingHistoryAmenity(int Quantity, string Name, decimal UnitPrice);
+public record BookingHistoryBeverage(int Quantity, string Name, decimal UnitPrice);
