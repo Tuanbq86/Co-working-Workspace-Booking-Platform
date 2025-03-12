a16a8d8cb752152525eda12c698aac0fbdfa4b4d
MokaMoka77 <tbuiquang103@gmail.com>
2025-03-12 16:35:01 +0700

    Build GetPolicies api

---
+++ WorkHive.APIs/Owner/ManagePolicy/GetPoliciesEndpoint.cs
@@ -0,0 +1,32 @@
+﻿using Carter;
+using MediatR;
+using WorkHive.Data.Models;
+using WorkHive.Services.Owners.ManageWorkSpace.Base_Policy;
+
+namespace WorkHive.APIs.Owner.ManagePolicy
+{
+    public record GetPoliciesResponse(List<Policy> Policies);
+    public class GetPoliciesEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/policies/", async (ISender sender) =>
+            {
+                var query = new GetAllPolicyQuery();
+                var result = await sender.Send(query);
+                if (result == null)
+                {
+                    return Results.Json(Array.Empty<GetPoliciesResponse>());
+                }
+                var response = new GetPoliciesResponse(result);
+                return Results.Ok(response);
+            })
+            .WithName("GetPolicies")
+            .Produces<GetPoliciesResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithTags("Policy")
+            .WithSummary("Get all policies ")
+            .WithDescription("Retrieve all policies.");
+        }
+    }
+}
--- WorkHive.Services/Owners/ManageImage/GetAllIById/GetImagesByFeedbackIdHandler.cs
+++ WorkHive.Services/Owners/ManageImage/GetAllIById/GetImagesByFeedbackIdHandler.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WorkHive.Services.Owners.ManageImage.GetAllIById
 {
-    class GetImagesByIdFeedbackHandler
+    class GetImagesByFeedbackIdHandler
     {
     }
 }
