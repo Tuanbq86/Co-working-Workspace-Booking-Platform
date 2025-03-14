b9969544c2c5a3540e94ae7376e5383d68a1f9ec
MokaMoka77 <tbuiquang103@gmail.com>
2025-03-15 03:54:32 +0700

    create get all promotion api

---
+++ WorkHive.APIs/Owner/ManagePromotion/GetPromotionsEndpoint.cs
@@ -0,0 +1,33 @@
+﻿using Carter;
+using MediatR;
+using WorkHive.Data.Models;
+using WorkHive.Services.Owners.ManageWorkSpace.Base_Promotion;
+
+namespace WorkHive.APIs.Owner.ManagePromotion
+{
+    public record GetPromotionsResponse(List<PromotionDT> Promotions);
+
+    public class GetPromotionsEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/promotions", async (ISender sender) =>
+            {
+                var query = new GetAllPromotionsQuery();
+                var result = await sender.Send(query);
+                if (result == null || !result.Any())
+                {
+                    return Results.Json(new GetPromotionsResponse(new List<PromotionDT>()));
+                }
+                var response = new GetPromotionsResponse(result);
+                return Results.Ok(response);
+            })
+            .WithName("GetPromotions")
+            .Produces<GetPromotionsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithTags("Promotion")
+            .WithSummary("Get all promotions")
+            .WithDescription("Retrieve all promotions.");
+        }
+    }
+}
