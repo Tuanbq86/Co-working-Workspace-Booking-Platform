b558cfb69c4e37b90a37112321575cabfc65ee68
Kobii24 <quochuyho220603@gmail.com>
2025-03-21 22:15:45 +0700

    add feature upload file pdf on cloudinary

---
+++ WorkHive.APIs/UploadImages/UploadFileEndpoint.cs
@@ -0,0 +1,39 @@
+﻿using Carter;
+using Mapster;
+using MediatR;
+using WorkHive.APIs.Owner.ManageWorkSpace.WorkSpace;
+using WorkHive.Services.UploadImages;
+
+namespace WorkHive.APIs.UploadImages;
+
+public record UploadFileRequest(List<IFormFile> Files);
+public record UploadFileResponse(int Status, string Message, List<string> Data);
+
+public class UploadFileEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/files/upload", async (HttpContext context, ISender sender) =>
+        {
+            //Đọc multipart/form-data từ request 
+            var form = await context.Request.ReadFormAsync();
+            //Lấy danh sách file từ form
+            var files = form.Files.ToList();
+            //Kiểm tra
+            if (files == null || files.Count == 0)
+                return Results.BadRequest("At least one file is required");
+            //Gửi Handler xử lý
+            var result = await sender.Send(new UploadFileCommand(files));
+            var response = result.Adapt<UploadFileResponse>();
+
+            return Results.Ok(response);
+        })
+        .Accepts<UploadFileRequest>("multipart/form-data")
+        .WithName("UploadFile")
+        .Produces<UploadFileResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Upload File")
+        .WithTags("Upload files on cloudinary")
+        .WithDescription("Upload File");
+    }
+}
