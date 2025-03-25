44ecc0b00e1e7133ead25361ae94426bbed376ac
MokaMoka77 <tbuiquang103@gmail.com>
2025-03-26 01:34:50 +0700

    build OwnerWithdrawalRequests API and update get users by owner id to have avatar

---
+++ WorkHive.Services/Staff/VerifyOwnerWithdrawalRequest/GetAllOwnerWithdrawalRequestsHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkHive.BuildingBlocks.CQRS;
+using WorkHive.Repositories.IUnitOfWork;
+
+namespace WorkHive.Services.Staff.VerifyOwnerWithdrawalRequest
+{
+    public record GetAllOwnerWithdrawalRequestsQuery() : IQuery<List<OwnerWithdrawalRequestDTO>>;
+
+    public record OwnerWithdrawalRequestDTO(int Id, string Title, string Description, string Status, DateTime? CreatedAt, int WorkspaceOwnerId, int UserId);
+
+    class GetAllOwnerWithdrawalRequestsHandler(IWalletUnitOfWork unit) : IQueryHandler<GetAllOwnerWithdrawalRequestsQuery, List<OwnerWithdrawalRequestDTO>>
+    {
+        public async Task<List<OwnerWithdrawalRequestDTO>> Handle(GetAllOwnerWithdrawalRequestsQuery query, CancellationToken cancellationToken)
+        {
+            var requests = await unit.OwnerWithdrawalRequest.GetAllAsync();
+            return requests.Select(r => new OwnerWithdrawalRequestDTO(r.Id, r.Title, r.Description, r.Status, r.CreatedAt, r.WorkspaceOwnerId, r.UserId)).ToList();
+        }
+    }
+}
