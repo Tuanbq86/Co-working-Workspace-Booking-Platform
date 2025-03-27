cc08a00bde9fdad3361155f03305358332382016
MokaMoka77 <tbuiquang103@gmail.com>
2025-03-28 01:47:27 +0700

    build UpdateOwnerWallet API

---
+++ WorkHive.Services/Wallets/Base_OwnerWallet/UpdateOwnerWalletHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkHive.BuildingBlocks.CQRS;
+using WorkHive.Repositories.IUnitOfWork;
+
+namespace WorkHive.Services.Wallets.Base_OwnerWallet
+{
+    public record UpdateOwnerWalletCommand(int Id, string BankName, string BankNumber, string BankAccountName) : IRequest<bool>;
+
+    public class UpdateOwnerWalletHandler(IWalletUnitOfWork unit) : IRequestHandler<UpdateOwnerWalletCommand, bool>
+    {
+        public async Task<bool> Handle(UpdateOwnerWalletCommand command, CancellationToken cancellationToken)
+        {
+            var ownerWallet = await unit.OwnerWallet.GetByIdAsync(command.Id);
+
+            if (ownerWallet == null)
+                return false;
+
+            ownerWallet.BankName = command.BankName;
+            ownerWallet.BankNumber = command.BankNumber;
+            ownerWallet.BankAccountName = command.BankAccountName;
+
+            await unit.SaveAsync();
+            return true;
+        }
+    }
+}
