564083f974268e8db1855efd8b5bd9dec33dca10
Ho Quoc Huy <quochuyho220603@gmail.com>
2025-04-12 13:36:59 +0700

    resolve conflict

---
+++ WorkHive.Repository/Repositories/CustomerWithdrawalRequestRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WorkHive.Data.Base;
+using WorkHive.Data.Models;
+using WorkHive.Repositories.IRepositories;
+
+namespace WorkHive.Repositories.Repositories;
+
+//public class CustomerWithdrawalRequestRepository : GenericRepository<CustomerWithdrawalRequest>, ICustomerWithdrawalRequestRepository
+//{
+//    public CustomerWithdrawalRequestRepository() { }
+//    public CustomerWithdrawalRequestRepository(WorkHiveContext context) => _context = context;
+//    public async Task<List<CustomerWithdrawalRequest>> GetByCustomerIdAsync(int customerId)
+//    {
+//        return await _context.CustomerWithdrawalRequests
+//            .Where(r => r.UserId == customerId)
+//            .ToListAsync();
+//    }
+//}
---
+++ WorkHive.Services/Managers/VerifyCustomerWithdrawalRequest/CreateCustomerWithdrawalRequestHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WorkHive.BuildingBlocks.CQRS;
+
+namespace WorkHive.Services.Managers.VerifyCustomerWithdrawalRequest;
+
+public record CreateCustomerWithdrawalRequestCommand(string Title, string Description, int CustomerId) : ICommand<CreateCustomerWithdrawalRequestResult>;
+public record CreateCustomerWithdrawalRequestResult(string Notification);
+
+public class CreateCustomerWithdrawalRequestHandler
+{
+}
