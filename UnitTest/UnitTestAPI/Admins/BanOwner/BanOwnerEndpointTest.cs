1221faa7d9c97643637fcc3e6cd76eedd5935776
Ho Quoc Huy <quochuyho220603@gmail.com>
2025-04-23 10:26:49 +0700

    done test ban customer endpoint

---
+++ UnitTest/UnitTestAPI/Admins/BanOwner/BanOwnerEndpointTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestAPI.Admins.BanOwner;
+
+public class BanOwnerEndpointTest
+{
+}
