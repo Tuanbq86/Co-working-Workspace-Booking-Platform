22bd173b0703ccc84539c3186d0e3221d41bfb46
Ho Quoc Huy <quochuyho220603@gmail.com>
2025-04-24 13:22:46 +0700

    add scenario for role or role

--- WorkHive.Services/DependencyInjection.cs
+++ WorkHive.Services/DependencyInjection.cs
@@ -64,6 +64,9 @@ public static class DependencyInjection
             op.AddPolicy("Staff", policy => policy.RequireClaim("RoleId", "3"));
             op.AddPolicy("Customer", policy => policy.RequireClaim("RoleId", "4"));
             op.AddPolicy("Owner", policy => policy.RequireClaim("RoleId", "5"));
+
+            //Hoặc có role này hoặc có role kia
+            op.AddPolicy("OwnerOrAdmin", policy => policy.RequireClaim("RoleId", "1", "5"));
         });
 
         //Save session into Ram when I restart all data will delete
