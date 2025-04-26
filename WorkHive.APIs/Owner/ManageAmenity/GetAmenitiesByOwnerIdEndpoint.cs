e9f0164c4836db72258b83e2ad3c6f1d41a29853
MokaMoka77 <tbuiquang103@gmail.com>
2025-04-26 15:58:33 +0700

    update

--- WorkHive.APIs/Owner/ManageAmenity/GetAmenitiesByOwnerIdEndpoint.cs
+++ WorkHive.APIs/Owner/ManageAmenity/GetAmenitiesByOwnerIdEndpoint.cs
@@ -31,7 +31,7 @@ namespace WorkHive.APIs.Owner.ManageAmenity
             .ProducesProblem(StatusCodes.Status404NotFound)
             .WithTags("Amenity")
             .WithSummary("Get Amenities by Owner ID")
-            .RequireAuthorization("Owner")
+            //.RequireAuthorization("Owner")
             .WithDescription("Retrieve all amenities belonging to a specific Owner.");
         }
     }
