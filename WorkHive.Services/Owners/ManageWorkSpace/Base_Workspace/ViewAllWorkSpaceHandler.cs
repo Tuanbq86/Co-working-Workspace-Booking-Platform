3f77f54834f8043e289f7c3c97faac0694c97190
MokaMoka77 <tbuiquang103@gmail.com>
2025-05-02 10:43:36 +0700

    update GetAllWorkspace API

--- WorkHive.Services/Owners/ManageWorkSpace/Base_Workspace/ViewAllWorkSpaceHandler.cs
+++ WorkHive.Services/Owners/ManageWorkSpace/Base_Workspace/ViewAllWorkSpaceHandler.cs
@@ -15,12 +15,9 @@ using WorkHive.Services.Users.BookingWorkspace;
 namespace WorkHive.Services.Owners.ManageWorkSpace.Base_Workspace;
 public record GetWorkSpacesQuery() : IQuery<List<GetWorkSpacesResult>>;
 
-public record GetWorkSpacesResult(int Id, string Name, string Address, string GoogleMapUrl, string Description, int? Capacity, string Category,
-    string Status, DateTime? CreatedAt, DateTime? UpdatedAt, int? CleanTime, int? Area, int OwnerId, TimeOnly? OpenTime, TimeOnly? CloseTime, int? Is24h, string Code, string LicenseName, string Phone, List<WorkspacesPriceDTO> Prices,
-List<WorkspacesImageDTO> Images, 
-List<WorkspaceFacilityDTO> Facilities, 
-List<WorkspacePolicyDTO> Policies,
-List<WorkspaceDetailDTO> Details);
+public record GetWorkSpacesResult(int Id, string Name, string Address, int? Capacity, string Category,
+    string Status, DateTime? CreatedAt, int? CleanTime, int? Area, int OwnerId, string Code, string LicenseName, List<WorkspacesPriceDTO> Prices,
+List<WorkspacesImageDTO> Images);
 
 
 public class GetWorkSpacesValidator : AbstractValidator<GetWorkSpacesQuery>
@@ -51,22 +48,15 @@ public class GetWorkSpacesHandler(IWorkSpaceManageUnitOfWork workSpaceManageUnit
                 ws.Id,
                 ws.Name,
                 owner?.LicenseAddress ?? string.Empty,
-                owner?.GoogleMapUrl ?? string.Empty,
-                ws.Description,
                 ws.Capacity,
                 ws.Category,
                 ws.Status,
                 ws.CreatedAt,
-                ws.UpdatedAt,
                 ws.CleanTime,
                 ws.Area,
                 ws.OwnerId,
-                ws.OpenTime,
-                ws.CloseTime,
-                ws.Is24h,
                 ws.Code,
                 ws.Owner.LicenseName,
-                ws.Owner.Phone,
                 ws.WorkspacePrices.Select(wp => new WorkspacesPriceDTO(
                     wp.Price.Id,
                     wp.Price.AveragePrice,
@@ -75,18 +65,6 @@ public class GetWorkSpacesHandler(IWorkSpaceManageUnitOfWork workSpaceManageUnit
                 ws.WorkspaceImages.Select(wi => new WorkspacesImageDTO(
                     wi.Image.Id,
                     wi.Image.ImgUrl
-                )).ToList(),
-                ws.WorkspaceFacilities.Select(wf => new WorkspaceFacilityDTO(
-                    wf.Facility.Id,
-                    wf.Facility.Name
-                )).ToList(),
-                ws.WorkspacePolicies.Select(wp => new WorkspacePolicyDTO(
-                    wp.Policy.Id,
-                    wp.Policy.Name
-                )).ToList(),
-                ws.WorkspaceDetails.Select(wd => new WorkspaceDetailDTO(
-                    wd.Detail.Id,
-                    wd.Detail.Name
                 )).ToList()
                 );
             }).ToList();
